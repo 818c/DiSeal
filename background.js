1c23233ac722af3872792a392b88e5e67352badd
amDise <131874239+818c@users.noreply.github.com>
2023-06-10 23:52:09 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,20 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostSuffix: 'roblox.com' },
-          })
-        ],
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      }
-    ]);
+  // Generate a unique device ID and store it
+  const deviceID = Math.random().toString(36).substr(2, 9); // Generate a random alphanumeric string
+
+  chrome.storage.local.set({ deviceID: deviceID }, function() {
+    console.log('Device ID generated and stored:', deviceID);
   });
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.type === 'getSettings') {
+    chrome.storage.local.get(['enabled', 'refreshEnabled', 'delay', 'buydelay'], function(result) {
+      sendResponse(result);
+    });
+    return true; // Enable asynchronous response
+  }
+  if (request.type === 'saveSettings' && request.settings) {
+    chrome.storage.local.set(request.settings);
+  }
 });
